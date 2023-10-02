1c71867068a00cee3e5bfa9a01e75bc02b786159
derya <derya@DERYA>
2023-10-02 18:22:25 +0300

    Add project files.

---
+++ Freelancer/Common/EntityBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Freelancer.Common
+{
+    internal class EntityBase<Tkey>
+    {
+        public Tkey Id { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+
+    }
+}
---
+++ Freelancer/Program.cs
@@ -0,0 +1,38 @@
+﻿using Freelancer.Entities;
+using Freelancer.Services;
+using Freelancer.Common;
+using Freelancer.Abstract;
+
+namespace Freelancer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var anotherCustomerInstance = new Customer
+            {
+                Id = Guid.NewGuid(),
+                CreatedOn = DateTime.Now,
+                FirstName = "Derya",
+                LastName = "Gelmez",
+                PhoneNumber = "544-123-4567"
+            };
+
+            NotepadService notepadService = new();
+
+            string customerData = notepadService.GetOnNotepad("C:\\Users\\derya\\source\\repos\\Freelancer\\Freelancer\\Database\\Customers.txt");
+            
+            string[] splittedLines = customerData.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+
+            List<Customer> customers = new();
+
+            foreach (var line in splittedLines)
+            {
+                Customer customer = new();
+                customer.SetValuesCSV(line);
+                customers.Add(customer);
+            }
+        }
+    }
+}
